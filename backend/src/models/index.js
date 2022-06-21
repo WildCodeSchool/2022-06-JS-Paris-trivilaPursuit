700cb804dd0034b58aa5723708c8ca0b4724ecdd
Marina Alves <ad.marina@hotmail.fr>
2022-06-21 15:19:58 +0200

    first commit API creation

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -4,21 +4,21 @@ const path = require("path");
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
-const pool = mysql.createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-});
+//const pool = mysql.createPool({
+//   host: DB_HOST,
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   database: DB_NAME,
+// }); 
 
-pool.getConnection().catch(() => {
-  console.warn(
-    "Warning:",
-    "Failed to get a DB connection.",
-    "Did you create a .env file with valid credentials?",
-    "Routes using models won't work as intended"
-  );
-});
+// pool.getConnection().catch(() => {
+//   console.warn(
+//     "Warning:",
+//     "Failed to get a DB connection.",
+//     "Did you create a .env file with valid credentials?",
+//     "Routes using models won't work as intended"
+//   );
+// });
 
 const models = fs
   .readdirSync(__dirname)
--- backend/src/router.js
+++ backend/src/router.js
@@ -4,10 +4,12 @@ const { ItemController } = require("./controllers");
 
 const router = express.Router();
 
-router.get("/items", ItemController.browse);
-router.get("/items/:id", ItemController.read);
-router.put("/items/:id", ItemController.edit);
-router.post("/items", ItemController.add);
-router.delete("/items/:id", ItemController.delete);
+//router.get("/items", ItemController.browse);
+router.get("/questions", ItemController.getQuestions);
+// la route des questions : "http://localhost:5000/questions"
+// router.get("/items/:id", ItemController.read);
+// router.put("/items/:id", ItemController.edit);
+// router.post("/items", ItemController.add);
+// router.delete("/items/:id", ItemController.delete);
 
 module.exports = router;
